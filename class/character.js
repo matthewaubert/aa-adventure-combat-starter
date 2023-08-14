f5c5bffd85796704989bffdb4f20f5a9b4cc1aa1
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-14 12:39:17 -0400

    Create Character class

--- class/character.js
+++ class/character.js
@@ -2,15 +2,26 @@ class Character {
 
   constructor(name, description, currentRoom) {
     // Fill this in
+    this.name = name;
+    this.description = description;
+    this.currentRoom = currentRoom;
 
+    this.items = [];
+    this.health = 100;
+    this.strength = 10;
   }
 
   applyDamage(amount) {
     // Fill this in
+    this.health -= amount; // subtract amount from character health
+    if (this.health <= 0) this.die(); // if character health <= 0, character dies
   }
 
   die() {
     // Fill this in
+    this.currentRoom.items.push(...this.items); // place all items in room
+    this.items = []; // drop all items
+    this.currentRoom = null; // set currentRoom to null
   }
 
 }
